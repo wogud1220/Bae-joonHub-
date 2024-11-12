f93bc0f7ab7dd1ad80f23e93b81ab79018e1e21a
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2024-11-12 12:50:40 +0900

    [Bronze IV] Title: 세수정렬, Time: 196 ms, Memory: 18420 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+        for(int i = 0; i<3; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < 3; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+    }
