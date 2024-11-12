5c1d22237170c792d718095f2bd36a1f9ec9c6ed
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2024-11-12 19:07:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        System.out.println((int)ch);
+        }
+}
