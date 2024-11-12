acca5a323bcb15a93f78cd6ae83fc4f03d30ce96
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2024-11-12 13:06:37 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+        System.out.println(n+"??!");
+
+    }
+
+    }
