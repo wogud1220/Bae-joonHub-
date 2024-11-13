3eee35373fff0a743799e7473b0e7496ed860117
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2024-11-13 18:48:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 2240 ms, Memory: 322340 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[0]+" ");
+        System.out.print(arr[N-1]);
+    }
+}
