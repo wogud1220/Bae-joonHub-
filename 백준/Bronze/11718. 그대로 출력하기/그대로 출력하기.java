8ab38a2c929b93522ec1ed089f5c06a47c93e79d
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2024-11-14 16:59:25 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 188 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+
+    }
+}
