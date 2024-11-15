932750afdffd2c87b7211ffccb93c019c3c669ca
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2024-11-15 19:15:53 +0900

    [Bronze V] Title: A/B, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println((double)N/b);
+
+    }
+}
