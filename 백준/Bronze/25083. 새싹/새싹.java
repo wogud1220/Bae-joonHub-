f96ce29f5a91e045ea8752ba28d144721444d377
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2024-11-15 17:35:24 +0900

    [Bronze V] Title: 새싹, Time: 160 ms, Memory: 17116 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
