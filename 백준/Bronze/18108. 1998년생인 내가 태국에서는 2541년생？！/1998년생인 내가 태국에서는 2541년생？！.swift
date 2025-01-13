21630a9a018adf2f6949904250dfb436777b5625
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2025-01-14 02:10:34 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 윤재형 on 1/14/25.
+//
+
+import Foundation
+
+var num = readLine()!.split(separator: " ").map{ Int(String($0))!}
+//var str = readLine()!
+
+print(num[0] - 543)
+
