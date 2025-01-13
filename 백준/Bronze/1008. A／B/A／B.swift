b28d2032688ec8d447c5b17c5703b554ec23b738
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2025-01-14 01:56:48 +0900

    [Bronze V] Title: A/B, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 윤재형 on 1/14/25.
+//
+
+import Foundation
+
+var num = readLine()!.split(separator: " ").map{ Double(String($0))!}
+
+print(num[0]/num[1])
