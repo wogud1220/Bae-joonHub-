3a09a56b0e00a8c4a0d85f624759b17b5bae40e7
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2025-01-14 02:01:14 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 윤재형 on 1/14/25.
+//
+
+import Foundation
+
+var num = readLine()!.split(separator: " ").map{ Int(String($0))!}
+
+print(num[0] + num[1])
+print(num[0] - num[1])
+print(num[0] * num[1])
+print(num[0] / num[1])
+print(num[0] % num[1])
