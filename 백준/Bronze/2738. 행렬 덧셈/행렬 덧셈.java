49c919f242f49b62d3e3a4cc8df6389d436d4bd4
JaeHyeong Yoon <102245864+wogud1220@users.noreply.github.com>
2025-02-04 00:51:46 -0800

    [Bronze III] Title: 행렬 덧셈, Time: 484 ms, Memory: 35852 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] array1 = new int[N][M];
+
+
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<M; j++){
+                array1[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] array2 = new int[N][M];
+
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<M; j++){
+                array2[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i<N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(array1[i][j] + +array2[i][j]+" ");
+            }
+            System.out.println("");
+        }
+
+
+
+
+
+        }
+
+    }
