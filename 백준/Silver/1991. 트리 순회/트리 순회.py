7c09c4e47a46952d7a4ba5d8434dc9f531012de9
JaeHyeong Yoon <yoonwogud@gmail.com>
2026-04-05 12:14:28 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 35644 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,51 @@
+import sys
+from collections import deque
+import heapq
+
+input = sys.stdin.readline
+
+
+
+N = int(input())
+tree = {}
+
+
+for i in range(N):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+
+def preorder(node):
+
+    if node == '.':
+        return
+
+    print(node, end= '')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
+
+
+def inorder(node):
+    if node == '.':
+        return
+
+    inorder(tree[node][0])
+
+    print(node, end = '')
+    inorder(tree[node][1])
+
+def postorder(node):
+    if node == '.':
+        return
+
+    postorder(tree[node][0])
+    postorder(tree[node][1])
+    print(node, end = '')
+
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
